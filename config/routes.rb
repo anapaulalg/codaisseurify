ce4c8f5ff1a9fc8a73956db9ecfd7080584114bf
anapaulalg <ana_paula_lg@hotmail.com>
2017-09-09 12:08:34 +0200

    Add nav bar and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
   devise_for :users
   root "artists#index"
 
   get "artists" => "artists#index"
   get "artists/:id" => "artists#show", as: :artist
+
+  root to: 'pages#home'
+  devise_for :users
 end
