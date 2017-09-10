1abddebb4622b80336ff1a481cf2b48bdabcdbd7
anapaulalg <ana_paula_lg@hotmail.com>
2017-09-10 13:04:00 +0200

    Install FactoryGirl, Faker and define factories

---
+++ spec/factories/artists.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :artist do
+    artist_name      { Faker::Lorem.sentence(30) }
+    country          { Faker::Lorem.sentence(30) }
+    user             { build(:user) }
+end
+
+  factory :song do
+    song_title      { Faker::Lorem.sentence(30) }
+    duration        { Faker::Lorem.sentence(30) }
+    youtube_link    { Faker::Lorem.sentence(30) }
+  end
+end
