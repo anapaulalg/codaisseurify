7b71cb77997069ed5f878eb13b343e8b39ff26cf
anapaulalg <ana_paula_lg@hotmail.com>
2017-09-17 16:45:02 +0200

    Removing youtube link from songs

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20170914073429) do
   create_table "songs", force: :cascade do |t|
     t.string "title"
     t.string "duration"
-    t.string "youtube_link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "artist_id"
